5114302e28a0b4395f3ab199f5fd77bf1edb082a
NoticeMeCapcake <danko3033@gmail.com>
2024-04-06 01:13:05 +0300

    added LRUCache

---
+++ LRUCache.py
@@ -0,0 +1,27 @@
+from collections import OrderedDict
+
+
+class LRUCache:
+    def __init__(self, capacity: int = 10) -> None:
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key: str) -> str:
+        if key not in self.cache:
+            return ""
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def set(self, key: str, value: str) -> None:
+        if key in self.cache:
+            self.cache[key] = value
+            self.cache.move_to_end(key)
+            return
+
+        self.cache[key] = value
+        if len(self.cache) == self.capacity:
+            self.cache.popitem(last=False)
+
+    def rem(self, key: str) -> None:
+        if key in self.cache:
+            self.cache.pop(key)
