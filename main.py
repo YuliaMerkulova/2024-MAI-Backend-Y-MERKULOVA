c583d749ef6e95277d2014d582c6273b4bacc065
NoticeMeCapcake <danko3033@gmail.com>
2024-04-06 01:13:30 +0300

    added checking LRUCache

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+from LRUCache import LRUCache
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+cache = LRUCache(100)
+cache.set('Jesse', 'Pinkman')
+cache.set('Walter', 'White')
+cache.set('Jesse', 'James')
+assert cache.get('Jesse') == 'James'
+cache.rem('Walter')
+assert cache.get('Walter') == ''
